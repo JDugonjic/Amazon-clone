339877fb146d1468fc9f1550075e56a1cb4f0a63
JDugonjic <jelenadugonjic@gmail.com>
2020-10-22 14:20:31 +0200

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDyzL6jjtva4finDrv3vSinRbBR-P2PLP0",
+    authDomain: "clone-55b41.firebaseapp.com",
+    databaseURL: "https://clone-55b41.firebaseio.com",
+    projectId: "clone-55b41",
+    storageBucket: "clone-55b41.appspot.com",
+    messagingSenderId: "1079924657240",
+    appId: "1:1079924657240:web:4881ceeec1126732818e2d"
+  };
