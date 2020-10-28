ac325e7a4e1001aadd1e913204bc0ee4ed4f6581
JDugonjic <jelenadugonjic@gmail.com>
2020-10-28 15:54:51 +0100

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from 'react';
-
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Home from "./Home"
 
 function App() {
   return (
-        <div className="app">
-
-        <h1>Hello this is Amazon clone app </h1>
-    
+    <div className="app">
+      <Header />
+      <Home />
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./Home.css";
+import Product from "./Product";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2020/PrimeDay/Fuji_TallHero_NonPrime_v2_en_US_1x._CB403670067_.jpg"
+          alt=""
+        />
+        <div className="home__row">
+          <Product
+            title="The lean startup"
+            price={19.99}
+            image="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg"
+            rating={5}
+          />
+          <Product />
+        </div>
+        <div className="home__row">
+          <Product />
+          <Product />
+          <Product />
+        </div>
+        <div className="home__row">
+          <Product />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
