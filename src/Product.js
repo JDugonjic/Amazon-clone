c8bcee128184f5ce925e52c1ed91d2731b13e987
JDugonjic <jelenadugonjic@gmail.com>
2020-10-31 15:46:48 +0100

    4th commit - added Basket functionality with React Context API

--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,24 @@
 import React from "react";
 import "./Product.css";
+import { useStateValue } from "./StateProvider";
 
 function Product({ id, title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  const addToBasket = () => {
+    //dispatch the item to data layer
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+
   return (
     <div className="product">
       <div className="product__info">
@@ -18,11 +35,8 @@ function Product({ id, title, image, price, rating }) {
             ))}
         </div>
       </div>
-      <img
-        src={image}
-        alt=""
-      />
-      <button>Add to Basket</button>
+      <img src={image} alt="" />
+      <button onClick={addToBasket}>Add to Basket</button>
     </div>
   );
 }
